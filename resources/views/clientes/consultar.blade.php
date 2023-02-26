9edf8c6814996bc21fb5e23dd7a87f1671221af8
Mateus M <mlcm.public@gmail.com>
2023-02-26 01:24:04 -0300

    fix: desativando o datatables

--- resources/views/clientes/consultar.blade.php
+++ resources/views/clientes/consultar.blade.php
@@ -63,12 +63,12 @@
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Lista de clientes</h6>
                             
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <table class="table table-bordered" id="" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
                                             <th>Opções</th>
